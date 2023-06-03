05811f6540e1c99ee8212db3ba9740d596edc3ea
Asif Mehmood <asif-mehmood@hotmail.com>
2023-06-03 23:35:51 +0500

    complete signup with google

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const passport = require('passport');
+const path = require('path');
+const cors = require('cors');
+const session = require('express-session');
+const app = express();
+require('dotenv').config();
+require('./auth');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client', 'index.html'));
+});
+
+app.use(
+  session({
+    secret: 'my secret key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/auth/google/success',
+    failureRedirect: '/auth/google/failure',
+  })
+);
+
+app.get('/auth/google/failure', isLoggedIn, (req, res) => {
+  res.send('Something went wrong!');
+});
+
+app.get('/auth/google/success', isLoggedIn, (req, res) => {
+  const name = req.user.displayName;
+  return res.status(200).send(`hello there! ${name}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is running on ${process.env.PORT}`);
+});
